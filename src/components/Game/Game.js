e2c35cc7edca2f0a2aa3dd6beb36568da4a25625
Peter De Genaro <pdegenaro@gmail.com>
2017-01-26 11:58:33 -0500

    add http get for game data

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,12 +3,28 @@ import React from 'react'
 import Timer from '../Timer/Timer'
 import styles from './Game.scss'
 
+const http = require('http')
+
 class Game extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      gameData: {},
+    }
+  }
+
+  componentDidMount() {
+    http.get('https://web-code-test-dot-nyt-games-prd.appspot.com/cards.json', (response) => {
+      response.setEncoding('utf8')
+      response.on('data', data => this.setState({ gameData: data }))
+    })
+  }
+
   render() {
     return (
       <div>
-        <h1 className={styles.header}>NYT Games Code Test</h1>
-        <Timer />
+        <h1 className={styles.header}>Memory</h1>
         <div className={styles.placeholder}>Let the games begin (here).</div>
       </div>
     )
