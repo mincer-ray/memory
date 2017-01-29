3f092f4198d8e8984806aa160bbab78ff36de9b6
Peter De Genaro <pdegenaro@gmail.com>
2017-01-28 19:40:21 -0500

    add scores component and firebase

---
+++ src/components/Scores/Scores.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import styles from './Scores.scss'
+import { formatTime } from '../Timer/Timer'
+
+export const sortScores = (scores) => {
+  const scoreArray = []
+  Object.keys(scores).forEach(score => scoreArray.push(Object.assign(scores[score], { id: score })))
+  return scoreArray.sort((a, b) => a.time - b.time)
+}
+
+const Score = ({ data = {} }) => {
+  return (
+    <tr>
+      <td>{data.name}</td>
+      <td>{data.attempts}</td>
+      <td>{formatTime(data.time)}</td>
+    </tr>
+  )
+}
+
+const Scores = ({ scores = [] }) => {
+  const formattedScores = []
+  sortScores(scores).forEach(score => formattedScores.push(<Score key={score.id} data={score} />))
+  return (
+    <table className={styles.scores}>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Attempts</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {formattedScores}
+      </tbody>
+    </table>
+  )
+}
+
+export default Scores
